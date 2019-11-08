860e1ddfadf8a6e4b5630239350bc1c0f83cb8de
chris meyers <chris.meyers.fsu@gmail.com>
2019-11-08 10:36:39 -0500

    POC channels 2

--- awx/main/management/commands/run_dispatcher.py
+++ awx/main/management/commands/run_dispatcher.py
@@ -8,12 +8,11 @@ from django.conf import settings
 from django.core.cache import cache as django_cache
 from django.core.management.base import BaseCommand
 from django.db import connection as django_connection, connections
-from kombu import Exchange, Queue
+from kombu import Exchange, Queue, Connection
 
 from awx.main.utils.handlers import AWXProxyHandler
 from awx.main.dispatch import get_local_queuename, reaper
 from awx.main.dispatch.control import Control
-from awx.main.dispatch.kombu import Connection
 from awx.main.dispatch.pool import AutoscalePool
 from awx.main.dispatch.worker import AWXConsumer, TaskWorker
 
@@ -74,6 +73,7 @@ class Command(BaseCommand):
         sched_file = '/var/lib/awx/beat.db'
         app = Celery()
         app.conf.BROKER_URL = settings.BROKER_URL
+        app.conf.BROKER_TRANSPORT_OPTIONS = settings.BROKER_TRANSPORT_OPTIONS
         app.conf.CELERY_TASK_RESULT_EXPIRES = False
 
         # celery in py3 seems to have a bug where the celerybeat schedule
@@ -128,7 +128,7 @@ class Command(BaseCommand):
         # in cpython itself:
         # https://bugs.python.org/issue37429
         AWXProxyHandler.disable()
-        with Connection(settings.BROKER_URL) as conn:
+        with Connection(settings.BROKER_URL, transport_options=settings.BROKER_TRANSPORT_OPTIONS) as conn:
             try:
                 bcast = 'tower_broadcast_all'
                 queues = [
