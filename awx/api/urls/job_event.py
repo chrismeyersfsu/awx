388962d7958d2ecca35cc8aec3f50c01a27f8ed5
Ryan Petrello <rpetrell@redhat.com>
2021-03-01 13:37:09 -0500

    remove the global `/api/v2/job_events/` endpoint

--- awx/api/urls/job_event.py
+++ awx/api/urls/job_event.py
@@ -4,7 +4,6 @@
 from django.conf.urls import url
 
 from awx.api.views import (
-    JobEventList,
     JobEventDetail,
     JobEventChildrenList,
     JobEventHostsList,
@@ -12,7 +11,6 @@ from awx.api.views import (
 
 
 urls = [
-    url(r'^$', JobEventList.as_view(), name='job_event_list'),
     url(r'^(?P<pk>[0-9]+)/$', JobEventDetail.as_view(), name='job_event_detail'),
     url(r'^(?P<pk>[0-9]+)/children/$', JobEventChildrenList.as_view(), name='job_event_children_list'),
     url(r'^(?P<pk>[0-9]+)/hosts/$', JobEventHostsList.as_view(), name='job_event_hosts_list'),
